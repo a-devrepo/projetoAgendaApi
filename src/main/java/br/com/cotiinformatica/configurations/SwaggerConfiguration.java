793ddad128f8c402bac19ee1e4206ab19afa5a41
alison <adevpr11@gmail.com>
2025-05-20 10:38:09 -0300

    Adiciona classe SwaggerConfiguration

---
+++ src/main/java/br/com/cotiinformatica/configurations/SwaggerConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.cotiinformatica.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("API Agenda")
+                .version("1.0")
+                .description("Documentação da API para gerenciamento de tarefas")
+                .contact(new Contact()
+                    .name("Seu Nome")
+                    .email("alison@email.com")
+                    .url("https://github.com/a-devrepo"))
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
