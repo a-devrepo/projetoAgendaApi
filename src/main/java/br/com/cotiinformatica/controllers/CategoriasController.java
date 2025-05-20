05261258007934eccc0e69629061c624b3b6893d
alison <adevpr11@gmail.com>
2025-05-20 10:14:13 -0300

    Adiciona anotações de mapeamento nas classes de controle

--- src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
@@ -1,5 +1,10 @@
 package br.com.cotiinformatica.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categorias")
 public class CategoriasController {
 
 }
--- src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
@@ -1,5 +1,10 @@
 package br.com.cotiinformatica.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tarefas")
 public class TarefasController {
 
 }
