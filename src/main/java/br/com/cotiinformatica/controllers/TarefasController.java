8c46f1d8b404d0d31f49706618b9cff852d8f7a6
alison <adevpr11@gmail.com>
2025-05-20 10:24:23 -0300

    Adiciona método post na classe TarefasController

--- src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
@@ -1,5 +1,6 @@
 package br.com.cotiinformatica.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/tarefas")
 public class TarefasController {
 
+	@PostMapping
+	public void post() {
+		// TODO: implementar o método para incluir uma tarefa
+	}
 }
