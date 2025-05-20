64e5fb7bffb25221ecb4c2625c476e131f41a4e9
alison <adevpr11@gmail.com>
2025-05-20 10:53:48 -0300

    Adiciona, em TarefasController, endpoints e mapeamento da documentação swagger

--- src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
@@ -1,15 +1,42 @@
 package br.com.cotiinformatica.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/tarefas")
+@Tag(name = "Controle de tarefas", 
+	description = "Serviços para gerenciamento de tarefas da agenda.")
 public class TarefasController {
 
 	@PostMapping
+	@Operation(summary = "Cadastro de tarefas", description = "Cria uma nova tarefa no sistema.")
 	public void post() {
-		// TODO: implementar o método para incluir uma tarefa
+		// TODO Implementar o serviço para cadastro de tarefa
+	}
+
+	@PutMapping
+	@Operation(summary = "Edição de tarefas", description = "Atualiza uma tarefa existente no sistema.")
+	public void put() {
+		// TODO Implementar o serviço para atualização de tarefa
+	}
+
+	@DeleteMapping
+	@Operation(summary = "Exclusão de tarefas", description = "Apaga uma tarefa no sistema.")
+	public void delete() {
+		// TODO Implementar o serviço para exclusão de tarefa
+	}
+
+	@GetMapping
+	@Operation(summary = "Consulta de tarefas", description = "Retorna as tarefas cadastradas no sistema..")
+	public void get() {
+		// TODO Implementar o serviço para consulta de tarefa
 	}
 }
