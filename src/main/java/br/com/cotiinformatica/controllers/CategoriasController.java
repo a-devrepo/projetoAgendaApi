7c18222fe3000a61b68823d6d75581b7832a7b7f
alison <adevpr11@gmail.com>
2025-05-22 10:10:44 -0300

    Altera método get de CategoriasController para usar o Repository

--- src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
@@ -1,9 +1,13 @@
 package br.com.cotiinformatica.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cotiinformatica.entities.Categoria;
+import br.com.cotiinformatica.repositories.CategoriaRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,10 +16,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Controle de categorias", description = "Serviços para gerenciamento de dados de categorias.")
 public class CategoriasController {
 
+	private CategoriaRepository repository;
+
+	public CategoriasController(CategoriaRepository repository) {
+		this.repository = repository;
+	}
+
 	@GetMapping
-	@Operation(summary = "Consulta de categorias", 
-	description = "Retorna todas as categorias cadastradas no sistema.")
-	public void get() {
-		// TODO implementar o serviço para consulta de categorias
+	@Operation(summary = "Consulta de categorias", description = "Retorna todas as categorias cadastradas no sistema.")
+	public List<Categoria> get() {
+		return repository.findAll();
 	}
 }
