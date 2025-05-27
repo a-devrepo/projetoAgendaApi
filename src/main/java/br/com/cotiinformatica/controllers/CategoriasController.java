054334382514150b191496cbc76e298d7c202a66
alison <adevpr11@gmail.com>
2025-05-27 11:17:20 -0300

    Adiciona ao método get lógica para criar lista de CategoriaResponseDTO

--- src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/CategoriasController.java
@@ -2,11 +2,12 @@ package br.com.cotiinformatica.controllers;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.cotiinformatica.entities.Categoria;
+import br.com.cotiinformatica.dtos.CategoriaResponseDTO;
 import br.com.cotiinformatica.repositories.CategoriaRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -24,7 +25,14 @@ public class CategoriasController {
 
 	@GetMapping
 	@Operation(summary = "Consulta de categorias", description = "Retorna todas as categorias cadastradas no sistema.")
-	public List<Categoria> get() {
-		return repository.findAll();
+	public List<CategoriaResponseDTO> get() {
+
+		var categorias = repository.findAll();
+		var mapper = new ModelMapper();
+
+		return categorias.stream()
+				.map(categoria -> 
+				mapper.map(categoria, CategoriaResponseDTO.class))
+				.toList();
 	}
 }
