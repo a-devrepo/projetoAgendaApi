4496c2edff298022e2296c34c905a196333ae8f0
alison <adevpr11@gmail.com>
2025-05-27 12:30:25 -0300

    Adiciona anotação de validação ao parâmetro do método post

--- src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
+++ src/main/java/br/com/cotiinformatica/controllers/TarefasController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cotiinformatica.dtos.TarefaRequestDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/tarefas")
@@ -18,7 +21,7 @@ public class TarefasController {
 
 	@PostMapping
 	@Operation(summary = "Cadastro de tarefas", description = "Cria uma nova tarefa no sistema.")
-	public void post() {
+	public void post(@RequestBody @Valid TarefaRequestDTO tarefaRequestDTO) {
 		// TODO Implementar o serviço para cadastro de tarefa
 	}
 
