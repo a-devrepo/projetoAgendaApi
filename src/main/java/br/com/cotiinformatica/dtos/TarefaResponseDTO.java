893cd6f099831122c62b98db7d28269a1583a369
alison <adevpr11@gmail.com>
2025-05-29 14:59:48 -0300

    Adiciona classe TarefaResponseDTO

---
+++ src/main/java/br/com/cotiinformatica/dtos/TarefaResponseDTO.java
@@ -0,0 +1,61 @@
+package br.com.cotiinformatica.dtos;
+
+import java.util.UUID;
+
+public class TarefaResponseDTO {
+
+	private UUID id;
+	private String titulo;
+	private String data;
+	private String hora;
+	private Boolean finalizado;
+	private CategoriaResponseDTO categoria;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	public CategoriaResponseDTO getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaResponseDTO categoria) {
+		this.categoria = categoria;
+	}
+}
